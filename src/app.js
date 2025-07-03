c51f2e8fca64a3b44e38d339f3abdbb660aa7834
DESKTOP-VN3VINC\Дмитро <kukuliak.dmytro.clg@chnu.edu.ua>
2025-07-03 21:08:30 +0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import healthRoutes from "./routes/healthRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
@@ -7,6 +9,9 @@ import inviteRoutes from "./routes/inviteRoutes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+dotenv.config();
 
 app.use("/auth", authRoutes);
 app.use("/", healthRoutes);
